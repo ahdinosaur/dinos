07fe4ad2690148e2db09fdbaca04550cde3e4210
Michael Williams <michael.williams@enspiral.com>
2017-07-29 00:21:18 +1200

    google chrome

--- config/web/index.js
+++ config/web/index.js
@@ -13,5 +13,10 @@ module.exports = [
 //      'xul-ext-noscript',
       'deluge-gtk'
     ]
+  },
+  // google-chrome
+  {
+    type: 'exec',
+    command: 'if ! [ -e "/etc/apt/sources.list.d/google-chrome.list" ]; then mkdir ~/tmp; cd ~/tmp; wget "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb" -O google-chrome.deb ; sudo dpkg -i google-chrome.deb; sudo apt install -yf; fi'
   }
 ]
