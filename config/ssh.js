6a5ccc1552a855fcfcdc24a3faca119a0c3112f0
Michael Williams <michael.williams@enspiral.com>
2020-03-22 13:20:03 +1300

    yep

--- config/ssh.js
+++ config/ssh.js
@@ -8,9 +8,9 @@ module.exports = [
   // https://security.stackexchange.com/questions/143442/what-are-ssh-keygen-best-practices
   {
     type: 'exec',
-    command: [
-      'if ! [ -e "~/.ssh/id_rsa" ]; then ssh-keygen -t ed25519 -a 100; fi',
-      'if ! [ -e "~/.ssh/id_ed25519" ]; then ssh-keygen -t rsa -b 4096 -o -a 100; fi'
+    commands: [
+      'if ! [ -e "~/.ssh/id_ed25519" ]; then ssh-keygen -t ed25519 -a 100 -f "~/.ssh/id_ed25519" -N ""; fi',
+      'if ! [ -e "~/.ssh/id_rsa" ]; then ssh-keygen -t rsa -b 4096 -o -a 100 -f "~/.ssh/id_rsa" -N ""; fi'
     ]
   }
 ]
