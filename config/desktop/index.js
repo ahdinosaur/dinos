257b342d8524c67931c1c2a35909616b336f0ef7
Michael Williams <michael.williams@enspiral.com>
2020-03-22 14:14:02 +1300

    yep

--- config/desktop/index.js
+++ config/desktop/index.js
@@ -1,21 +1,28 @@
 const { join } = require('path')
 
 module.exports = [
+  // https://regolith-linux.org/docs/getting-started/install/
+  {
+    type: 'aptsource',
+    name: 'ppa:regolith-linux/release'
+  },
   {
     type: 'aptpkg',
     packages: [
+      'regolith-desktop',
       'redshift-gtk'
     ]
-  }
-  /*
+  },
   {
     type: 'exec',
-    command: `mkdir -p $HOME/.config/regolith/i3`
+    commands: [
+      'mkdir -p $HOME/.config/regolith/i3',
+      'mkdir -p $HOME/.config/regolith/i3xrocks'
+    ]
   },
   {
     type: 'link',
     source: join(__dirname, 'i3/config'),
     target: '.config/regolith/i3/config'
-  },
-  */
+  }
 ]
