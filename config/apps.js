4307abdb246c56ae9536e98bf6e9ebce2a63a7a1
Michael Williams <michael.williams@enspiral.com>
2020-11-15 01:32:33 +1300

    yep

--- config/apps.js
+++ config/apps.js
@@ -52,7 +52,7 @@ module.exports = [
   {
     type: 'exec',
     commands: [
-      'curl -fsSL https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -',
+      'sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90',
       'echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list',
       'sudo apt update'
     ]
